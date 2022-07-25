1073cc28699e1910300662d68546605fc0c91571
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-07-25 23:16:57 +0300

    Add dashboard and profile in base urls

--- to_do_app/base/admin.py
+++ to_do_app/base/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
 from to_do_app.base.models import Profile
+from to_do_app.task.models import Task
+
+
+class TasksInlineAdmin(admin.StackedInline):
+    model = Task
+    extra = 1
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'day_of_birth')
+    inlines = (TasksInlineAdmin, )
--- to_do_app/base/views.py
+++ to_do_app/base/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+from to_do_app.base.models import Profile
+from to_do_app.task.models import Project, Task
+
+
+def get_profile():
+    profile = Profile.objects.all()
+    if profile:
+        return profile[0]
+    return None
+
+
+def get_tasks():
+    task_list = True
+
 
 def home_page(request):
     context = {
@@ -10,10 +24,30 @@ def home_page(request):
 
 
 def dashboard_page(request):
+    profile = get_profile()
+    profile_full_name = f'{profile.first_name} {profile.last_name}'
+    task_count = profile.task_set.count()
+    projects = Project.objects.all()
+    # tasks_list = profile.task_set.filter(project__task__user_profile_id=profile)
+    # tasks_list = profile.task_set.all().filter(project__task__title__in=projects)
+    # tasks_list = profile.task_set.filter(project__title__in=projects)
+    tasks_list = Task.objects.filter(project__in=projects)
+    # task_count = tasks_list.count()
     context = {
-        'title': 'Dashboard'
+        'title': 'Dashboard',
+        'active_additions_nav_items': True,
+        'profile_full_name': profile_full_name,
+        'tasks_list': tasks_list,
+        'task_count': task_count,
+        'projects': projects,
     }
 
-    return render(request, '', context)
+    return render(request, 'dashboard.html', context)
 
 
+def profile_page(request):
+    context = {
+        'title': 'Profile'
+    }
+
+    return render(request, 'profile.html', context)
