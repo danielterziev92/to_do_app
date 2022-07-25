5a13656d251ee376d02a8ac5f98f5d5be979f26a
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-07-25 18:41:52 +0300

    Add dashboard and profile in base urls

--- to_do_app/base/urls.py
+++ to_do_app/base/urls.py
@@ -2,10 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from to_do_app.base.views import home_page, dashboard_page
+from to_do_app.base.views import home_page, dashboard_page, profile_page
 
 urlpatterns = [
     path('', home_page, name='home page'),
     path('dashboard/', dashboard_page, name='dashboard page'),
-
+    path('profile/', profile_page, name='profile page')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
