93abae1b734de32b37939cfc717962c5caff92b7
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-07-26 22:39:26 +0300

    add to task/urls.py path to get element of task id

--- to_do_app/base/urls.py
+++ to_do_app/base/urls.py
@@ -7,5 +7,5 @@ from to_do_app.base.views import home_page, dashboard_page, profile_page
 urlpatterns = [
     path('', home_page, name='home page'),
     path('dashboard/', dashboard_page, name='dashboard page'),
-    path('profile/', profile_page, name='profile page')
+    path('profile/', profile_page, name='profile page'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- to_do_app/task/urls.py
+++ to_do_app/task/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from to_do_app.task.views import tasks_list
 
 urlpatterns = (
-    path('', tasks_list, name='tasks list')
+    path('', tasks_list, name='task list'),
+    path('<int:pk>', task_info, name='task info'),
 )
