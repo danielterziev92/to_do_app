fed1e61b04ef0d89d72a06ed2081bb2df86daa58
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-07-26 22:39:59 +0300

    add in main url to path to task/url

--- to_do_app/urls.py
+++ to_do_app/urls.py
@@ -6,4 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('to_do_app.base.urls')),
+    path('task/', include('to_do_app.task.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
