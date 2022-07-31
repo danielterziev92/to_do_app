9339e1538a9def7ba6afdae6eda1d16f7f2afceb
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-07-31 22:22:22 +0300

    Finish this page

--- to_do_app/task/urls.py
+++ to_do_app/task/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from to_do_app.task.views import tasks_list
+from to_do_app.task.views import task_edit, task_create, task_delete, project_edit, \
+    project_delete, project_create
 
 urlpatterns = (
-    path('', tasks_list, name='task list'),
-    path('<int:pk>', task_info, name='task info'),
+    path('add/', task_create, name='task create'),
+    path('edit/<int:pk>/', task_edit, name='task edit'),
+    path('delete/<int:pk>/', task_delete, name='task delete'),
+
+    path('project/add', project_create, name='project create'),
+    path('project/edit/<int:pk>/', project_edit, name='project edit'),
+    path('project/delete/<int:pk>/', project_delete, name='project delete'),
 )
