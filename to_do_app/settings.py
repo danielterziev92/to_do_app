9e82ded350e071a7aa9e5ce85acf9d74a69b59bb
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-07-31 22:21:56 +0300

    Finish this page

--- to_do_app/settings.py
+++ to_do_app/settings.py
@@ -103,9 +103,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "templates/static/",
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'templates/media')
+MEDIA_URL = 'media/'
 
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'templates/media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
